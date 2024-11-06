bba036ef481de421bd5186a54a3f7dfbf0fb416f
thomas giroud <thomasgiroud07@gmail.com>
2024-11-06 12:46:04 +0100

    Interface graphique

---
+++ ProjetMIFAREClassic/mainwindow.cpp
@@ -0,0 +1,75 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QtGui>
+#include "MfErrNo.h"
+#include "Sw_Device.h"
+#include "Sw_ISO14443A-3.h"
+#include "Sw_Mf_Classic.h"
+#include "Tools.h"
+#include "TypeDefs.h"
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::MainWindow)
+{ 
+    ui->setupUi(this);
+    initPictures();
+    EnableConfiguration(false);
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+ReaderName MonLecteur;
+
+void MainWindow::on_ConnectButton_clicked()
+{
+    int16_t status = MI_OK;
+    MonLecteur.Type = ReaderCDC;
+    MonLecteur.device = 0;
+    status = OpenCOM(&MonLecteur);
+    qDebug() << "OpenCOM" << status;
+    ui->ConnectButton->setEnabled(false);
+    ui->DisconnectButton->setEnabled(true);
+    EnableConfiguration(true);
+
+    //status = Version(&MonLecteur);
+    //ui->Affichage->setText(MonLecteur.version);
+    //ui->Affichage->update();
+}
+void MainWindow::on_DisconnectButton_clicked()
+{
+    RF_Power_Control(&MonLecteur, FALSE, 0);
+    LEDBuzzer(&MonLecteur, LED_OFF);
+    CloseCOM(&MonLecteur);
+    ui->ConnectButton->setEnabled(true);
+    ui->DisconnectButton->setEnabled(false);
+    EnableConfiguration(false);
+}
+
+void MainWindow::initPictures()
+{
+    QLabel* logo1 = ui->Logo_1;
+    logo1->setPixmap(QPixmap("D:/Communication sans fil/ProjetMIFAREClassic/Img/OIG2.png"));
+
+    QLabel* logo2 = ui->Logo_2;
+    logo2->setPixmap(QPixmap("D:/Communication sans fil/ProjetMIFAREClassic/Img/OIG2.png"));
+
+}
+void MainWindow::EnableConfiguration(bool isvisible)
+{
+    QGroupBox * GroupeIdentite = ui->Identite;
+    GroupeIdentite->setEnabled(isvisible);
+
+    QGroupBox * GroupeIcrementation = ui->Incrementation;
+    GroupeIcrementation->setEnabled(isvisible);
+
+    QGroupBox * GroupeDecrementation = ui->Decrementation;
+    GroupeDecrementation->setEnabled(isvisible);
+
+
+}
---
+++ ProjetMIFAREClassic/mainwindow.h
@@ -0,0 +1,29 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QWidget
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    void initPictures();
+    void EnableConfiguration(bool isvisible);
+private slots:
+    void on_ConnectButton_clicked();
+    void on_DisconnectButton_clicked();
+
+};
+#endif // MAINWINDOW_H
